fc003a2b2c602dd765d2143220f456f15fb03da2
Evgeny Cherkashin <eugene@irnok.net>
2015-09-11 11:48:04 +0800

    Adding websvn.

---
+++ rcs-base/srv/httpd/websvn/include/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config->addTemplatePath($locwebsvnreal.'/templates/calm/');
+$config->addTemplatePath($locwebsvnreal.'/templates/BlueGrey/');
+$config->addTemplatePath($locwebsvnreal.'/templates/Elegant/');
+
+$config->addInlineMimeType('text/plain');
+
+$config->setMinDownloadLevel(2);
+
+$config->useGeshi();
+
+$config->expandTabsBy(8);
+
+$config->addRepository('ISDCT SVN repo', 'file:///svn/SVN');
