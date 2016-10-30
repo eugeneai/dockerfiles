71935821b165e0a3f68c542142ce64bc16352a0a
Evgeny Cherkashin <eugene@irnok.net>
2016-10-30 22:52:41 +0800

    Simplifying sudo.

--- python-pyenv/setup-user.sh
+++ python-pyenv/setup-user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pacman -Sy base-devel python-virtualenv --noconfirm --needed
-pacman -S sudo git --noconfirm --needed
-echo "python ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-useradd -m -u 900 -r -s /usr/bin/bash python
+# pacman -S sudo git --noconfirm --needed
+# echo "python ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd -m -u 900 -G wheel -r -s /usr/bin/bash python
 #groupadd -g 900 python
 chown python:python /home -R
 # Temporal password for ssh, should be changed
