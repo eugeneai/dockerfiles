7e2ed693f0ecae5caa3ec2c6081a0870933eea00
Evgeny Cherkashin <eugene@irnok.net>
2016-10-30 09:53:39 +0800

    Generating part of bashrc.

--- python-pyenv/setup-home.sh
+++ python-pyenv/setup-home.sh
@@ -5,9 +5,15 @@ export TERM=xterm
 
 yaourt -S pyenv --noconfirm --needed
 
-echo 'export TERM=xterm' >> $HOME/.bash_profile
 
 pyenv init
 git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
 
 # exec "$SHELL"
+# TERM=xterm-256color
+echo 'PATH=$PATH:~/bin'  >> $HOME/.bashrc
+echo 'alias mc=". /usr/lib/mc/mc-wrapper.sh"' >> $HOME/.bashrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+echo 'export TERM=xterm' >> $HOME/.bashrc
+
