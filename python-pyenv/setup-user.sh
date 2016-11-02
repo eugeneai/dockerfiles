760683a6e3c575f8a203ac5981b16fbff6d13444
Evgeny Cherkashin <eugene@irnok.net>
2016-11-02 17:36:34 +0800

    Development are secially condenced in a VM.

--- python-pyenv/setup-user.sh
+++ python-pyenv/setup-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pacman -Sy base-devel python-virtualenv --noconfirm --needed
+pacman -Sy python-virtualenv --noconfirm --needed
 # pacman -S sudo git --noconfirm --needed
 # echo "python ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 useradd -m -u 900 -G wheel -r -s /usr/bin/bash python
@@ -7,4 +7,4 @@ useradd -m -u 900 -G wheel -r -s /usr/bin/bash python
 chown python:python /home -R
 # Temporal password for ssh, should be changed
 echo "python:python111" | chpasswd 
-
+/root/root-cache-clean.sh
